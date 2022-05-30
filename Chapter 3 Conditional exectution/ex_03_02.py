c776f3d456fcb28d28fde3b8d1238a5b935d108f
steven-14 <104389629+steven-14@users.noreply.github.com>
2022-05-30 17:53:08 +0100

    Added example 3.2

---
+++ Chapter 3 Conditional exectution/ex_03_02.py
@@ -0,0 +1,21 @@
+'''Exercise 2: Use try except'''
+
+# Input the  hours and pay, then turn those inputs into floating point numbers (decimals)
+
+hours = input("Enter Hours: " )
+rate = input("Enter Rate: ")
+try:
+    fh = float(hours)
+    fr = float(rate)
+except:
+    print("Error, please enter numeric input")
+    
+print(fh, fr)
+if fh > 40:
+    regularpay = fh * fr
+    overtimepay = (fh - 40.0) * (fr * 0.5)
+    pay = regularpay + overtimepay
+else:
+    pay = fh * fr
+print("Pay:",pay)
+
