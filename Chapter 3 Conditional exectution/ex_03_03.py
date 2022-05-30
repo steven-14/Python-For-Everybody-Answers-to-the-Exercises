885f5a3b7a6dd3e76605ca49baa9b657793e339e
steven-14 <104389629+steven-14@users.noreply.github.com>
2022-05-30 21:14:08 +0100

    changed code. Works on autograder exercise 3.3

--- Chapter 3 Conditional exectution/ex_03_03.py
+++ Chapter 3 Conditional exectution/ex_03_03.py
@@ -1,20 +1,11 @@
 # Exercise 3.3: Write a program to a grade to a score
 
-score = input('Please enter a score between 0.0 and 1: ')
-try:
-    floatscore = float(score)
-except:
-    print('Please enter a numerical value')
- 
-if floatscore < 0 or floatscore > 1:
-        print('Error. Please enter a number between 1.0 and 2.0')
-elif floatscore >= 0.9:
-        print('Your grade is A')
-elif floatscore >= 0.8:
-        print('Your grade is B')
-elif floatscore >= 0.7:
-        print('Your grade is C')
-elif floatscore > 0.6:
-        print('Your grade is D')
-elif floatscore < 0.6:
-        print('Your grade is F')
+score = input('Enter your grade ') 
+fscore = float(score) 
+if fscore < 0 or fscore > 1:
+    print('Error. Please enter a number between 1.0 and 2.0')
+elif fscore >= 0.9: print('A') 
+elif fscore >= 0.8: print('B') 
+elif fscore >= 0.7: print('C') 
+elif fscore >= 0.6: print('D') 
+elif fscore < 0.6: print('F') 
