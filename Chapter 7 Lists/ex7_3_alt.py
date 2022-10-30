fe5c38bec0df6ddd704fa0578d3d4ea454b7e203
steven-14 <104389629+steven-14@users.noreply.github.com>
2022-10-30 21:36:42 +0000

    Update ex7_3_alt.py

--- Chapter 7 Lists/ex7_3_alt.py
+++ Chapter 7 Lists/ex7_3_alt.py
@@ -1,23 +1,9 @@
 # Exercise 7.3. This version does not calculate the confidence. Soluton 7_3_v1 calculates the confidence also.
 
 """
-Exercise  7.3: Sometimes when programmers get vored or want to have a bit of
-fun, they adda harmless Easter Egg to their program. Modify the program that
-prompts the user for a file name so that is prints a funny message when the
-the user types in the exact file name "na na boo boo". the program should
-behave normally for all other files which exist and don't exit. Here is a
-sample execution of the program:
-python egg.py
-Enter the file name: mbox.txt
-There were 1797 subject lines in mbox.txt
-python egg.py
-Enter the file name: missing.tyxt
-File cannot be opened: missing.tyxt
-python egg.py
-Enter the file name: na na boo boo
-NA NA BOO BOO TO YOU - You have been punk'd!
-Python for Everybody: Exploring Data Using Python 3
-by Charles R. Severance
+Exercise  7.3: 
+Modify the program that prompts the user for a file name so that is prints a funny message when the the user types in the exact file name "na na boo boo". the program should
+behave normally for all other files which exist and don't exit.
 """
 
 fname = input('Enter the file name: ')
