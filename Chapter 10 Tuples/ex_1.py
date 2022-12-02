644c9067a54e0d4dc464c57896c4a23a55e45b5a
steven-14 <104389629+steven-14@users.noreply.github.com>
2022-12-02 12:28:59 +0000

    Create ex_1.py

---
+++ Chapter 10 Tuples/ex_1.py
@@ -0,0 +1,30 @@
+
+# Pull out the addresses from the lines in a file.
+
+dictionary_addresses = dict()           # Initialize variables
+lst = list()
+
+fname = input('Enter file name: ')
+try:
+    fhand = open(fname)
+except FileNotFoundError:
+    print('File cannot be opened:', fname)
+    quit()
+
+for line in fhand:
+    words = line.split()
+    if len(words) < 2 or words[0] != 'From':
+        continue
+    else:
+        if words[1] not in dictionary_addresses:
+            dictionary_addresses[words[1]] = 1       # First entry
+        else:
+            dictionary_addresses[words[1]] += 1      # Additional counts
+
+for key, val in list(dictionary_addresses.items()):
+    lst.append((val, key))              # Fills list with value, key of dict
+
+lst.sort(reverse=True)                  # Sorts by highest value
+
+for count, email in lst[:1]:            # Only displays the largest value
+    print(email, count)
