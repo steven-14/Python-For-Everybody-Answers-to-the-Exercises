5c96cc468291c489a4243ca514beb0c421190326
steven-14 <104389629+steven-14@users.noreply.github.com>
2022-12-02 12:31:51 +0000

    Create ex_2.py

---
+++ Chapter 10 Tuples/ex_2.py
@@ -0,0 +1,32 @@
+
+# Print the hour that each message was received.
+
+dictionary_hours = dict()               # Initialize variables
+lst = list()
+
+fname = input('Enter file name: ')
+try:
+    fhand = open(fname)
+except FileNotFoundError:
+    print('File cannot be opened:', fname)
+    quit()
+
+for line in fhand:
+    words = line.split()
+    if len(words) < 5 or words[0] != 'From':
+        continue
+
+    col_pos = words[5].find(':')
+    hour = words[5][:col_pos]
+    if hour not in dictionary_hours:
+        dictionary_hours[hour] = 1      # First entry
+    else:
+        dictionary_hours[hour] += 1     # Additional counts
+
+for key, val in list(dictionary_hours.items()):
+    lst.append((key, val))              # Fills list with hour, count of dict
+
+lst.sort()                              # Sorts by hour
+
+for key, val in lst:
+    print(key, val)
